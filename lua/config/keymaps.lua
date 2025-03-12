f0a339f327cb40e72ac29749e74bb9c89ce361a6
Noah Klinger <46035153+NoahSK23@users.noreply.github.com>
2025-03-12 12:29:59 -0400

    Added buffer = true to scroll, changed description

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,5 +8,5 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>a", "ggVG", { desc = "Select all" })
 
 --- Center cursor when moving down/up half-page
-keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
-keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center cursor", buffer = true })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center cursor", buffer = true })
