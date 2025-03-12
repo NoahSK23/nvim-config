694b4b27467044bd9820256d57bd278c8f52f227
Noah Klinger <46035153+NoahSK23@users.noreply.github.com>
2025-03-12 09:43:06 -0400

    Added center cursor while moving down/up keymap
    Updated plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,8 @@
 local keymap = vim.keymap
 
 --- Select all text
-keymap.set("n", "<leader>a", "ggVG")
+keymap.set("n", "<leader>a", "ggVG", { desc = "Select all" })
+
+--- Center cursor when moving down/up half-page
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
